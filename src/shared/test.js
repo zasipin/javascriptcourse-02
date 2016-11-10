6bfced01d773023ed3d9b467f405ebff9a1130de
ZasypinNV <zasipin@rambler.ru>
2016-11-10 16:54:54 +0300

    less 1

---
+++ src/shared/test.js
@@ -0,0 +1,3 @@
+export function blegh() {
+    console.log("It works");
+}
---
+++ webpack.server.js
@@ -0,0 +1,28 @@
+var path = require("path");
+
+function createConfig(isDebug) {
+    // WEBPACK CONFIG
+    return {
+        target: "node",
+        devtool: "source-map",
+        entry: "./src/server/server.js",
+        output: {
+            path: path.join(__dirname, "build"),
+            filename: "server.js"
+        },
+        resolve: {
+            alias: {
+                shared: path.join(__dirname, "src", "shared")
+            }
+        },
+        module: {
+            loaders: [
+                { test: /\.js$/, loader: "babel", exclude: /node_modules/ },
+                { test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/ }
+            ]
+        }
+    };
+}
+
+module.exports = createConfig(true);
+module.exports.create = createConfig;
